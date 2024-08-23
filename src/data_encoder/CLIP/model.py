09cdbc995d25c882d8636a778d273a981d473b14
tooniesnguyen <nnnhan1882002@gmail.com>
2024-08-23 19:38:42 +0700

    update temp

--- src/data_encoder/CLIP/model.py
+++ src/data_encoder/CLIP/model.py
@@ -4,6 +4,8 @@ from PIL import Image
 import clip
 import torch
 import numpy as np
+import glob
+from tqdm import tqdm
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[3]
@@ -12,6 +14,9 @@ if str(ROOT) not in sys.path:
     
 from src.abstraction.encoder_model import EncoderModel
 
+from src.abstraction.store_db import StoreDB
+from src.utils.logger import register
+logger = register.get_tracking("CLIP.implement.py")
 
 class CLIPModel(EncoderModel):
     def __init__(self, device: str) -> None:
@@ -37,3 +42,26 @@ class CLIPModel(EncoderModel):
         image_feat = image_feat.detach().cpu().numpy().astype(np.float16).flatten() 
         
         return image_feat
+    
+    def convert_image2npy(self, images_path, npy_path):
+        video_paths = sorted(glob.glob(f"{images_path}/*/"))
+        video_paths = ['/'.join(i.split('/')[:-1]) for i in video_paths]
+        
+        re_feats = []
+        for vd_path in video_paths:
+            keyframe_paths = glob.glob(f'{vd_path}/*.jpg')
+            keyframe_paths = sorted(keyframe_paths, key=lambda x : x.split('/')[-1].replace('.jpg',''))
+            
+            for keyframe_path in tqdm(keyframe_paths):
+                image_feat = self.image_encoder(keyframe_path)
+
+                re_feats.append(image_feat)
+                
+        name_npy = video_paths[0].split('/')[-2]
+        outfile = f'{npy_path}/{name_npy}.npy'
+        np.save(outfile, re_feats)
+        logger.info(f"Saved at path {outfile}")
+        return None
+    
+    def convert_text2npy(self, text_file, npy_path):
+        pass
--- src/data_preprocess/REAME.md
+++ src/data_preprocess/REAME.md
@@ -9,21 +9,21 @@ pip install -r requirements.txt
 ### Step 1: Convert Video to Keyframes
 ```
 cd Autoshot
-python implement.py --input_dir <path_to_Keyframes_L0*> \
-                    --output_dir <path_to_images>
+python implement.py --i <path_to_Keyframes_L0*> \
+                    --o <path_to_images>
 ```
 #### Example:
 ```
-python implement.py --input_dir /home/.../data/videos/Keyframes_L02 \
-                    --output_dir /home/.../data/images
+python implement.py --i /home/.../data/videos/Keyframes_L02 \
+                    --o /home/.../data/images
 ```
 
 ### Step 2: Clean data (check duplicate, dark, blurry, ...)
 ```
 cd clean_data
-python clean_vision.py --input_dir <path_to_images>
+python clean_vision.py --i <path_to_images>
 ```
 #### Example:
 ```
-python clean_vision.py --input_dir /home/.../data/images/Keyframes_L01
+python clean_vision.py --i /home/.../data/images/Keyframes_L01
 ```
